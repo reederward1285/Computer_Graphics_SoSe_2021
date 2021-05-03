62e57c532c2750de2ca8f1bb2c7ff12305793210
Dawinartor <46395323+Dawinartor@users.noreply.github.com>
2021-05-03 14:47:25 +0200

    Add print method to vertex class

--- Assignments/Week 4/HomeworkEveryone/vertex.cpp
+++ Assignments/Week 4/HomeworkEveryone/vertex.cpp
@@ -11,7 +11,8 @@
 
 //NEW
 #include "vertex.h"
-
+#include <iostream>
+using namespace std;
 
 Vertex::Vertex()
 {
@@ -30,4 +31,9 @@ Vertex::Vertex( float point[3])
     xyz[0] = point[0];
     xyz[1] = point[1];
     xyz[2] = point[2];
+}
+
+void Vertex::print(string description = "Vertex = ")
+{
+    cout << description << "[ " << xyz[0] << "; " << xyz[1] << "; " << xyz[2] << " ]" << endl;
 }
